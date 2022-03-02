b59cb4a6832b61bbae15a06d1798070534f369c8
JM09 <jassimbackup62@gmail.com>
2022-03-02 19:33:39 +0300

    added name

---
+++ jasem/Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Jassim alajmi on 02/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, my name is Jasem")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ jasem/Shared/jasemApp.swift
@@ -0,0 +1,17 @@
+//
+//  jasemApp.swift
+//  Shared
+//
+//  Created by Jassim alajmi on 02/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct jasemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
